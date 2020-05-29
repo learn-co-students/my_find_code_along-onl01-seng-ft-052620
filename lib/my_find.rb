43c2e96b95dfca4b0078efd404f5f74b7007f295
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-29 18:33:40 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  index = 0 
+  
+  while index < collection.length 
+    if yield(collection[index])
+      return collection [index]
+    
+    end
+    return collection[index] if yield(collection[index])
+    index += 1
+  end 
 end
